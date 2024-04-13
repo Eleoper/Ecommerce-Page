44b1390b2c7703af421d98de746cf0c78a120a93
Eleoper <123415922+Eleoper@users.noreply.github.com>
2024-04-12 23:51:30 -0500

    Add files via upload

---
+++ Ecommerce Website/Ecommerce Website/myscripts.js
@@ -0,0 +1,10 @@
+
+
+//To open and close side bar
+function openNav(){
+    document.getElementById("mySideBar").style.width = "250px";
+}
+
+function closeNav(){
+    document.getElementById("mySideBar").style.width = "0";
+}
